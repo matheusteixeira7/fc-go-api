b49c3d71d32be5c508b23b7960d40983078e9085
Matheus Teixeira <matheus.teixeira7@gmail.com>
2022-10-12 19:50:29 -0300

    fix: FindByID method

--- internal/infra/database/product_db.go
+++ internal/infra/database/product_db.go
@@ -34,16 +34,18 @@ func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error
 	return products, err
 }
 
-func (p *Product) FindById(id string) (*entity.Product, error) {
+func (p *Product) FindByID(id string) (*entity.Product, error) {
 	var product entity.Product
+	err := p.DB.First(&product, "id = ?", id).Error
+	if err != nil {
+		return nil, err
+	}
 
-	err := p.DB.First(&product, id).Error
-
-	return &product, err
+	return &product, nil
 }
 
 func (p *Product) Update(product *entity.Product) error {
-	_, err := p.FindById(product.ID.String())
+	_, err := p.FindByID(product.ID.String())
 	if err != nil {
 		return err
 	}
@@ -52,7 +54,7 @@ func (p *Product) Update(product *entity.Product) error {
 }
 
 func (p *Product) Delete(id string) error {
-	product, err := p.FindById(id)
+	product, err := p.FindByID(id)
 	if err != nil {
 		return err
 	}
